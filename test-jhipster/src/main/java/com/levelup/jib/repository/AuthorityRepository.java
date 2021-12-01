7cf3a1c6f2af2af65ed2244823a31f2d5b1c9959
saba <moslehsaba@gmail.com>
2021-12-01 15:39:08 +0330

    Initial version of jhipster generated by generator-jhipster@7.4.0

---
+++ test-jhipster/src/main/java/com/levelup/jib/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.levelup.jib.repository;
+
+import com.levelup.jib.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
