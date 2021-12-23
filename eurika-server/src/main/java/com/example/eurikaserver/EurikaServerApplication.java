7cb020c510366ee9508e47629aa655d63fb6688f
saba <moslehsaba@gmail.com>
2021-12-23 17:59:38 +0330

    initial commit

---
+++ eurika-server/src/main/java/com/example/eurikaserver/EurikaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurikaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaServerApplication.class, args);
+    }
+
+}
